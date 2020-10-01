8a07822e9b89b0263dca75972aa18342520d20e8
Meredith McIntosh <meredith@mcintosh.io>
2020-10-01 15:25:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ end
 end
 
 def get_japanese_emoticon(path, emoticon)
-  load_library(path) do |key,value|
+  load_library(path).each do |key,value|
     if value[:english] == emoticon
       return value[:japanese]
    end
