54b86cda58733444dd22f33ef681ec60bddb0453
Meredith McIntosh <meredith@mcintosh.io>
2020-10-01 15:12:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,17 +16,17 @@ end
 def get_english_meaning(path, emoticon)
   load_library(path).each do |key,value|
   if value[:japanese] == emoticon
-    return key[:english]
+    return key
   end
 end
   return "Sorry, that emoticon was not found"
 end
 
 def get_japanese_emoticon(path, emoticon)
-  load_library(path) { |key,value|
+  load_library(path) do |key,value|
     if value[:english] == emoticon
       return value[:japanese]
    end
-  }
+  end
   return "Sorry, that emoticon was not found"
 end
