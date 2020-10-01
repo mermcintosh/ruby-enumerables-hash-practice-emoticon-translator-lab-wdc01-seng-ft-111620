6f637424253358a97feae3d8fbc68ead92a43572
Meredith McIntosh <meredith@mcintosh.io>
2020-10-01 13:54:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+require modules here
+
+def load_library(path)
+ final_hash = {}
+ YAML.load_file(path).each do |key, value|
+   binding.pry
+ end
+ final_hash
 end
 
 def get_japanese_emoticon
